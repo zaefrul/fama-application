85f773f6bed9f415453aca47d331c30d7ffca6d1
Zaefrul <zaefrul@gmail.com>
2026-08-20 01:26:18 +0800

    Update Next.js configuration to limit CPU usage and disable worker threads. Add new build script in package.json to set CPU count for optimized builds.

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,8 @@ const nextConfig: NextConfig = {
     ignoreDuringBuilds: true,
   },
   experimental: {
+    cpus: 1,
+    workerThreads: false,
     serverActions: {
       bodySizeLimit: "8mb",
     },
